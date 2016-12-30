3508116d828867a2cc44fb64b7cac779c30e6329
patnir <rahul20patni@gmail.com>
2016-12-30 22:07:20 +0530

    adding attribute parser

--- HackerrankSolutions/Strings/attributeParser.cpp
+++ HackerrankSolutions/Strings/attributeParser.cpp
@@ -7,8 +7,55 @@ using namespace std;
 
 #define NF "Not Found!\n";
 
-void parseLine(string s) {
-    cout << "Parsing line " << s << "\n";
+string getValue(string line) {
+    string val;
+    for (int i = 0; i < line.size() && val.size() == 0; i++) {
+        if (line[i] == '\"') {
+            int j = i + 1;
+            while (line[j] != '\"') {
+               val.push_back(line[j]);
+               j++;
+            }
+            return val;
+        }
+    }
+    return val;
+}
+
+
+string removeTag(string line) {
+    string tag;
+    return tag;
+}
+
+string removeAttribute(string line) {
+    string att;
+    return att;
+}
+
+
+vector<string> buildCombinations(vector<string> lines, vector<string> attributes) {
+    vector<string> combos;
+    string curr;
+    int total = lines.size();
+    for (int i = 0; i < total; i++) {
+        if (lines[i][1] != '/') {
+            if (curr.size() == 0) {
+                
+            }
+            else {
+                
+            }
+            string val = getValue(lines[i]);
+            attributes.push_back(val);
+            combos.push_back(curr);
+            cout << val;
+        }
+        else {
+            
+        }
+    }
+    return combos;
 }
 
 void parseQuery() {
@@ -25,6 +72,9 @@ int main() {
         if (i != 0)
             lines.push_back(s);
     }
+    
+    vector<string> attributes;
+    vector<string> combos = buildCombinations(lines, attributes);
     
     vector<string> queries;
     for (int i = 0; i < q; i++) {
